eb2b72c1d4ac42f31d1c59f819aff0b8df7374f6
Branislav Hozza <brankohozza@gmail.com>
2024-06-21 20:26:23 +0200

    feat: prepare label component

---
+++ components/dot/label.story.vue
@@ -0,0 +1,15 @@
+<template>
+  <Story title="Dot/Label" auto-props-disabled>
+    <Variant title="Default">
+      <dot-label text="Switch *" :required="required">
+        <dot-switch />
+      </dot-label>
+    </Variant>
+  </Story>
+</template>
+
+<script lang="ts" setup>
+const state = reactive({
+  required: false,
+});
+</script>
---
+++ components/dot/label.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex flex-col gap-2">
+    <label class="font-bold">{{ text }} <span v-if="required">*</span></label>
+    <slot />
+  </div>
+</template>
+
+<script lang="ts" setup>
+defineProps<{
+  text: string;
+  required?: boolean;
+}>();
+</script>
+
+<style></style>
