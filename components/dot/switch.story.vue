e66f0340004722c84589c2a419f1ced767e404e8
Branislav Hozza <brankohozza@gmail.com>
2024-06-21 20:14:24 +0200

    feat: switch story

---
+++ components/dot/switch.story.vue
@@ -0,0 +1,18 @@
+<template>
+  <Story title="Dot/Switch" auto-props-disabled>
+    <Variant title="Default">
+      <dot-switch v-model="state.check" :disabled="state.disabled" />
+    </Variant>
+    <template #controls>
+      <HstCheckbox v-model="state.check" title="Checked" />
+      <HstCheckbox v-model="state.disabled" title="Disabled" />
+    </template>
+  </Story>
+</template>
+
+<script lang="ts" setup>
+const state = reactive({
+  check: false,
+  disabled: false,
+});
+</script>
