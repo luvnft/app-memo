15b510f0e2fd2f6ab7e411f3003d74656a1c8a17
Branislav Hozza <brankohozza@gmail.com>
2024-06-21 20:31:58 +0200

    feat: vertical & error variant

--- components/dot/label.vue
+++ components/dot/label.vue
@@ -1,6 +1,11 @@
 <template>
-  <div class="flex flex-col gap-2">
-    <label class="font-bold">{{ text }} <span v-if="required">*</span></label>
+  <div
+    class="flex"
+    :class="vertical ? 'flex-row justify-between' : 'flex-col gap-2'"
+  >
+    <label class="font-bold" :class="{ 'text-k-red': error }"
+      >{{ text }} <span v-if="required">*</span></label
+    >
     <slot />
   </div>
 </template>
@@ -9,6 +14,8 @@
 defineProps<{
   text: string;
   required?: boolean;
+  vertical?: boolean;
+  error?: boolean;
 }>();
 </script>
 
