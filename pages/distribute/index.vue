3cdd1d9213680904424938611c3af646a62c3515
Hladikes <adi.hladik@gmail.com>
2024-08-01 22:07:21 +0200

    feat: added new form fields

--- pages/distribute/index.vue
+++ pages/distribute/index.vue
@@ -33,12 +33,77 @@
             <dot-text-input v-model="endDate" type="date" />
           </dot-label>
         </div>
+        <dot-label text="Quantity">
+          <dot-text-input
+            v-model.number="quantity"
+            type="number"
+            placeholder="0"
+          />
+          <div class="flex gap-2">
+            <dot-button
+              class="flex-1"
+              size="small"
+              variant="secondary-rounded"
+              @click="quantity -= 100"
+              >-100</dot-button
+            >
+            <dot-button
+              class="flex-1"
+              size="small"
+              variant="secondary-rounded"
+              @click="quantity -= 10"
+              >-10</dot-button
+            >
+            <dot-button
+              class="flex-1"
+              size="small"
+              variant="secondary-rounded"
+              @click="quantity -= 1"
+              >-1</dot-button
+            >
+            <dot-button
+              class="flex-1"
+              size="small"
+              variant="secondary-rounded"
+              @click="quantity = 0"
+              >0</dot-button
+            >
+            <dot-button
+              class="flex-1"
+              size="small"
+              variant="secondary-rounded"
+              @click="quantity += 1"
+              >+1</dot-button
+            >
+            <dot-button
+              class="flex-1"
+              size="small"
+              variant="secondary-rounded"
+              @click="quantity += 10"
+              >+10</dot-button
+            >
+            <dot-button
+              class="flex-1"
+              size="small"
+              variant="secondary-rounded"
+              @click="quantity += 100"
+              >+100</dot-button
+            >
+          </div>
+        </dot-label>
+        <dot-label text="POAP Secret">
+          <dot-text-input placeholder="event2024" />
+        </dot-label>
       </div>
     </div>
+    <dot-button size="large" variant="primary-shadow" class="w-full"
+      >Create</dot-button
+    >
   </div>
 </template>
 
 <script lang="ts" setup>
 const startDate = ref(new Date());
 const endDate = ref(new Date());
+const quantity = ref(0);
 </script>
