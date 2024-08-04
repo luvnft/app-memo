a681847373368a4a0c61690a46c77ccbb40b72c6
Hladikes <adi.hladik@gmail.com>
2024-08-04 16:42:03 +0200

    fix: adjusted footer color

--- components/layout/footer.vue
+++ components/layout/footer.vue
@@ -1,6 +1,6 @@
 <template>
   <footer
-    class="flex w-full justify-center border-t border-primary bg-primary/20 p-2"
+    class="flex w-full justify-center border-t border-k-primary bg-k-primary-light p-2 text-text-color"
   >
     .POAP 2024
   </footer>
