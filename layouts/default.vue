4b0ebc63fdfbd3e63adac6c3db8eb3ac439d57fa
Hladikes <adi.hladik@gmail.com>
2024-08-04 16:43:16 +0200

    fix: fixed layout height

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
-  <div id="app-base" class="relative min-h-screen bg-background-color pt-10">
+  <div id="app-base" class="relative bg-background-color pt-10">
     <layout-header />
-    <main class="h-full p-4">
+    <main class="h-full min-h-screen p-4">
       <slot />
     </main>
     <layout-footer />
