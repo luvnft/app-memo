084bfdd588425e053f83ba0a705b8d084eb1c70c
Hladikes <adi.hladik@gmail.com>
2024-08-04 16:40:09 +0200

    fix: fixed z-index

--- components/layout/header.vue
+++ components/layout/header.vue
@@ -1,6 +1,6 @@
 <template>
   <header
-    class="border-primary fixed left-0 top-0 flex h-[60px] w-full items-center justify-between border-b bg-white/[0.12] p-2 px-4 shadow-[0_4px_30px] shadow-black/[0.1] backdrop-blur-[10.2px]"
+    class="border-primary fixed left-0 top-0 z-50 flex h-[60px] w-full items-center justify-between border-b bg-white/[0.12] p-2 px-4 shadow-[0_4px_30px] shadow-black/[0.1] backdrop-blur-[10.2px]"
   >
     <nuxt-link to="/">
       <h1 class="text-2xl dark:text-white">.POAP</h1>
