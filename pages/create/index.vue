971affcbe4f9bf6cfdc2a20dac4a4ccf3dd2e8b6
Branislav Hozza <brankohozza@gmail.com>
2024-08-06 15:17:41 +0200

    feat: add validation

--- pages/create/index.vue
+++ pages/create/index.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="mx-auto flex max-w-4xl flex-col space-y-7 px-4 pt-8">
+  <form
+    class="mx-auto flex max-w-4xl flex-col space-y-7 px-4 pt-8"
+    @submit="onSubmit"
+  >
     <h1 class="text-4xl font-extrabold text-text-color">Distribution Tool</h1>
 
     <div class="grid grid-cols-3 gap-8">
@@ -13,6 +16,7 @@
             :limit="150"
             type="text"
             placeholder="XYZ Event Collection"
+            :error="titleError"
           />
         </dot-label>
         <dot-label text="POAP description">
@@ -20,17 +24,30 @@
             v-model="description"
             :limit="150"
             placeholder="Describe your POAP. These description will be displayed for all POAP users."
+            :error="descriptionError"
           />
         </dot-label>
         <dot-label text="Website address">
-          <dot-text-input v-model="websiteAddress" placeholder="https://" />
+          <dot-text-input
+            v-model="websiteAddress"
+            placeholder="https://"
+            :error="websiteAddressError"
+          />
         </dot-label>
         <div class="grid grid-cols-2 gap-8">
           <dot-label text="Start date">
-            <dot-text-input v-model="startDate" type="date" />
+            <dot-text-input
+              v-model="startDate"
+              type="date"
+              :error="startDateError || localStartDateError"
+            />
           </dot-label>
           <dot-label text="End">
-            <dot-text-input v-model="endDate" type="date" />
+            <dot-text-input
+              v-model="endDate"
+              type="date"
+              :error="endDateError || localEndDateError"
+            />
           </dot-label>
         </div>
         <dot-label text="Quantity">
@@ -38,6 +55,7 @@
             v-model.number="quantity"
             type="number"
             placeholder="0"
+            :error="quantityError"
           />
           <div class="flex gap-2">
             <dot-button
@@ -102,17 +120,97 @@
         </dot-label>
       </div>
     </div>
-    <dot-button size="large" variant="primary-shadow" class="w-full">
+    <dot-button
+      :disabled="!isSubmittable"
+      size="large"
+      submit
+      variant="primary-shadow"
+      class="w-full"
+    >
       Create
     </dot-button>
-  </div>
+  </form>
 </template>
 
 <script lang="ts" setup>
-const title = ref("");
-const description = ref("");
-const websiteAddress = ref("");
-const startDate = ref(new Date());
-const endDate = ref(new Date());
-const quantity = ref(0);
+import { useField, useForm } from "vee-validate";
+import { toTypedSchema } from "@vee-validate/zod";
+import * as zod from "zod";
+
+const validationSchema = toTypedSchema(
+  zod.object({
+    title: zod
+      .string({ message: "Title is required" })
+      .min(1, { message: "Title is required" }),
+    description: zod.string().optional(),
+    websiteAddress: zod
+      .string()
+      .url({ message: "URL has invalid format" })
+      .optional(),
+    startDate: zod.date({ message: "Start date is required" }),
+    endDate: zod.date({ message: "End date is required" }),
+    quantity: zod
+      .number({ message: "Quantity is required" })
+      .positive({ message: "Quantity must be positive" }),
+  }),
+);
+
+const { handleSubmit, errors } = useForm({
+  validationSchema,
+  initialValues: {
+    quantity: 1,
+  },
+});
+
+const { value: title, errorMessage: titleError } = useField<string>("title");
+const { value: description, errorMessage: descriptionError } =
+  useField<string>("description");
+const { value: websiteAddress, errorMessage: websiteAddressError } =
+  useField<string>("websiteAddress");
+const { value: startDate, errorMessage: startDateError } =
+  useField<Date>("startDate");
+const localStartDateError = ref<string>("");
+const { value: endDate, errorMessage: endDateError } =
+  useField<Date>("endDate");
+const localEndDateError = ref<string>("");
+const { value: quantity, errorMessage: quantityError } =
+  useField<number>("quantity");
+
+// As `refine` doesnt work with `toTypedSchema` we need to do this manually
+watch([startDate, endDate], ([startDate, endDate]) => {
+  if (startDate > endDate) {
+    localStartDateError.value = "Start date must be before end date";
+    localEndDateError.value = "End date must be after start date";
+  } else {
+    localStartDateError.value = "";
+    localEndDateError.value = "";
+  }
+});
+
+const onSubmit = handleSubmit(
+  ({ description, endDate, quantity, startDate, title, websiteAddress }) => {
+    if (localStartDateError.value || localEndDateError.value) {
+      return;
+    }
+    console.log({
+      description,
+      endDate,
+      quantity,
+      startDate,
+      title,
+      websiteAddress,
+    });
+  },
+);
+
+const isSubmittable = computed(
+  () =>
+    title.value &&
+    startDate.value &&
+    endDate.value &&
+    quantity.value &&
+    !localStartDateError.value &&
+    !localEndDateError.value &&
+    !Object.keys(errors.value).length,
+);
 </script>
