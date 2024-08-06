71ce542b46db8f99cde2bd2a04699833c3360321
Branislav Hozza <brankohozza@gmail.com>
2024-08-06 15:17:27 +0200

    fix: console issues

--- components/dot/text-area.vue
+++ components/dot/text-area.vue
@@ -4,15 +4,17 @@
       class="relative flex w-full items-center justify-between gap-2 border focus-within:border-k-primary focus-within:bg-k-primary-light"
       :class="wrapperClasses"
     >
-      <textarea
-        ref="textarea"
-        v-model="model"
-        :placeholder="placeholder"
-        :class="inputClasses"
-        :maxlength="limit"
-        class="min-h-20 min-w-0 flex-1 resize-y bg-transparent p-3 text-text-color focus:outline-none focus:ring-0"
-      >
-      </textarea>
+      <client-only>
+        <textarea
+          ref="textarea"
+          v-model="model"
+          :placeholder="placeholder"
+          :class="inputClasses"
+          :maxlength="limit"
+          class="min-h-20 min-w-0 flex-1 resize-y bg-transparent p-3 text-text-color focus:outline-none focus:ring-0"
+        >
+        </textarea>
+      </client-only>
 
       <span
         v-if="Number.isInteger(limit)"
