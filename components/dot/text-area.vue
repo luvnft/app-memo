9477ec7ccd1f5e7012412cfbf2a0ec3f525bde12
Hladikes <adi.hladik@gmail.com>
2024-08-18 14:27:11 +0200

    refactor: client-only removed

--- components/dot/text-area.vue
+++ components/dot/text-area.vue
@@ -4,18 +4,14 @@
       class="relative flex w-full items-center justify-between gap-2 border focus-within:border-k-primary focus-within:bg-k-primary-light"
       :class="wrapperClasses"
     >
-      <client-only>
-        <textarea
-          ref="textarea"
-          v-model="model"
-          :placeholder="placeholder"
-          :class="inputClasses"
-          :maxlength="limit"
-          class="min-h-20 min-w-0 flex-1 resize-y bg-transparent p-3 text-text-color focus:outline-none focus:ring-0"
-        >
-        </textarea>
-      </client-only>
-
+      <textarea
+        ref="textarea"
+        v-model="model"
+        :placeholder="placeholder"
+        :class="inputClasses"
+        :maxlength="limit"
+        class="min-h-20 min-w-0 flex-1 resize-y bg-transparent p-3 text-text-color focus:outline-none focus:ring-0"
+      />
       <span v-if="Number.isInteger(limit)" class="absolute -top-5 right-0 text-xs text-text-color">
         {{ model?.length ?? 0 }} / {{ limit }}
       </span>
