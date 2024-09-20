ee455d4a0b9cb05d256be03a23d80ad07b3a9b1f
Viki Val <viktorko99@gmail.com>
2024-09-20 16:40:03 +0200

    :technologist: useApi

---
+++ composables/useApi.ts
@@ -0,0 +1,25 @@
+import { ApiFactory } from "@kodadot1/sub-api";
+import type { ApiPromise } from "@polkadot/api";
+import { getChainEndpointByPrefix } from "@/utils/chain";
+
+export default function () {
+  // const { urlPrefix } = usePrefix()
+
+  const apiUrl = computed(() => {
+    const endpoint: string = getChainEndpointByPrefix("ahp") || "";
+    return endpoint;
+  });
+
+  const apiInstance = computed<Promise<ApiPromise>>(() => ApiFactory.useApiInstance(apiUrl.value));
+
+  const apiInstanceByPrefix = (prefix: string) => {
+    const endpoint: string = getChainEndpointByPrefix(prefix) || "";
+    return ApiFactory.useApiInstance(endpoint);
+  };
+
+  return {
+    apiUrl,
+    apiInstance,
+    apiInstanceByPrefix,
+  };
+}
