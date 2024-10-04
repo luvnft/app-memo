62b98168adf4eac7f84afe8eb929b7afac81c50f
Viki Val <viktorko99@gmail.com>
2024-10-04 16:59:39 +0200

    :pray: tell me the chain

--- components/layout/footer.vue
+++ components/layout/footer.vue
@@ -1,7 +1,10 @@
 <template>
-  <footer
-    class="flex w-full justify-center border-t border-k-primary bg-k-primary-light p-2 text-text-color"
-  >
-    .POAP 2024
+  <footer class="flex w-full justify-center border-t border-k-primary bg-k-primary-light p-2 text-text-color">
+    .POAP 2024 {{ name }}
   </footer>
 </template>
+
+<script lang="ts" setup>
+const { prefix } = usePrefix();
+const name = computed(() => (prefix.value === "ahp" ? "Polkadot" : "Kusama"));
+</script>
