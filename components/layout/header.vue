1371c63f1ce882f661330e4c7815c7138d8b7bd6
Hladikes <adi.hladik@gmail.com>
2024-10-12 14:49:33 +0200

    fix: added blur

--- components/layout/header.vue
+++ components/layout/header.vue
@@ -1,5 +1,7 @@
 <template>
-  <header class="border-primary fixed left-0 top-0 z-50 flex h-[60px] w-full items-center justify-between p-6">
+  <header
+    class="border-primary fixed left-0 top-0 z-50 flex h-[60px] w-full items-center justify-between p-6 backdrop-blur-sm"
+  >
     <nuxt-link to="/">
       <h1 class="text-2xl dark:text-white">.MEMO</h1>
     </nuxt-link>
